1a62e64bcda3f69c4342498d014630f9c896f462
Pararock <rogermokin999@gmail.com>
2016-09-04 20:25:30 -0400

    First Commit GUI Editor

---
+++ QSP/QSPSaveEditor/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using QSPSaveEditor.Model;
+
+namespace QSPSaveEditor.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData( Action<DataItem, Exception> callback )
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
